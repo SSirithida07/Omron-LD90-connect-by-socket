13a17fdee05a7bfbefc4a87741076ba17da236bd
ssirithida <113410593+SSirithida07@users.noreply.github.com>
2023-06-11 01:15:56 +0700

    code.py
    การเชื่อมOmron โดยSocketในPython

---
+++ code.py
@@ -0,0 +1,55 @@
+import socket
+
+# กำหนดค่า Host และ Port ของหุ่นยนต์
+HOST = '192.168.1.33'  
+PORT = 7171
+
+# สร้าง Socket Object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# เชื่อมต่อกับหุ่นยนต์
+s.connect((HOST, PORT))
+
+# รับข้อมูลจากหุ่นยนต์
+data = s.recv(1024)
+print('Received:', data)
+
+# ส่ง Password ให้กับหุ่นยนต์
+password = b'admin\n'
+s.sendall(password)
+
+# รับข้อมูลจากหุ่นยนต์
+data = s.recv(1024)
+print('Received:', data)
+
+while True:
+    # รับคำสั่งจากผู้ใช้
+    command = input("Enter ARCL command: ")
+
+    if command == 'exit':
+        break
+
+    if command == 'd':
+        command = b'dock\n'
+    elif command == 'ud':
+        command = b'undock\n'
+    elif command == 'go':
+            i = input("Goal where?: ")
+            command = b'goto goal' + i.encode() + b'\n'
+    elif command == 's':
+            command = b' stopped\n'
+
+    else:
+        print("Invalid command")
+        continue
+
+    # ส่งคำสั่ง ARCL ให้กับหุ่นยนต์
+    print(command)
+    s.sendall(command)
+
+    # รับข้อมูลจากหุ่นยนต์
+    data = s.recv(1024)
+    print('Received:', data.decode())
+
+
+
